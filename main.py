b4c20a0e8dbf2e65a4f9cc80191477ebce896441
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-02-29 19:44:39 -0300

    feat: encapsulate the logic in class

--- main.py
+++ main.py
@@ -6,54 +6,57 @@ load_dotenv()
 book_path = os.environ.get('BOOK_PATH')
 
 with open(book_path) as f:
-    file_contents = f.read()
+    book = f.read()
 
 
-def count_words(book_text):
-    total = 0
-    words = book_text.split()
+class Bookbot:
+    def __init__(self, book):
+        self.book = book
 
-    for word in words:
-        total += 1
+    def __count_words(self, book_text):
+        total = 0
+        words = book_text.split()
 
-    return total
+        for word in words:
+            total += 1
+
+        return total
+
+    def __count_letters(self, book_text):
+        letters = {}
+
+        for word in book_text:
+            word_characters = word.lower().split()
+
+            for character in word_characters:
+                is_letter = character.isalpha()
+
+                if is_letter:
+                    if letters.get(character) is None:
+                        letters[character] = 1
+                    else:
+                        letters[character] = letters.get(character) + 1
+
+        return letters
+
+    def generate_report(self):
+        count_letters_dic = self.__count_letters(self.book)
+        count_letters_list = list(count_letters_dic.keys())
+        count_letters_list.sort()
+
+        print(f"--- Begin report of {book_path} ---")
+
+        print(f"{self.__count_words(self.book)} words found in the document")
+
+        print()
+
+        for letter in count_letters_list:
+            print(
+                f"The {letter} character was found " +
+                f"{count_letters_dic[letter]} times")
+
+        print("--- End report ---")
 
 
-def count_letters(book_text):
-    letters = {}
-
-    for word in book_text:
-        word_characters = word.lower().split()
-
-        for character in word_characters:
-            is_letter = character.isalpha()
-
-            if is_letter:
-                if letters.get(character) is None:
-                    letters[character] = 1
-                else:
-                    letters[character] = letters.get(character) + 1
-
-    return letters
-
-
-def generate_report():
-    count_letters_dic = count_letters(file_contents)
-    count_letters_list = list(count_letters_dic.keys())
-    count_letters_list.sort()
-
-    print(f"--- Begin report of {book_path} ---")
-
-    print(f"{count_words(file_contents)} words found in the document")
-
-    print()
-
-    for letter in count_letters_list:
-        print(
-            f"The {letter} character was found " +
-            f"{count_letters_dic[letter]} times")
-
-    print("--- End report ---")
-
-
-generate_report()
+book_bot = Bookbot(book)
+book_bot.generate_report()
